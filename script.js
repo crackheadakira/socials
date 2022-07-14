0053ea0b3d0dcaa1b1f2e18549cd83f8adbea221
crackheadakira <lasn.mine@gmail.com>
2022-07-14 21:55:31 +0200

    Add background option to JSON config

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const socialLink = document.querySelector('#socialLink');
 const profile = document.querySelector('#profile');
 const title = document.querySelector('#title');
+const backgroundImg = document.querySelector('#main').style;
 
 getJSON().then(data => {
+    
+    backgroundImg.setProperty('--background', `url(${data.user.background})`);
     profile.src = data.user.profile;
     title.textContent = data.user.name;
+
     for (social in data.socials) {
         let items = Object.values(data.socials[social]);
         if (items[2] <= 0) { break }
@@ -13,7 +17,7 @@ getJSON().then(data => {
         a.href = items[1];
         a.target = "_blank";
         img.src = items[2];
-        img.alt = items[0];
+        img.alt = `${social} - ${items[0]}`
         img.classList.add("socialLink");
         a.appendChild(img);
         socialLink.appendChild(a);
