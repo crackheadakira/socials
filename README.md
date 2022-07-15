34fcfb090ee9fcd7a652cd6f5807af4724750ba3
crackheadakira <lasn.mine@gmail.com>
2022-07-15 20:08:54 +0200

    Add special look for Discord

---
+++ README.md
@@ -0,0 +1,11 @@
+# Socials
+This is a simple site I wrote to derust my webdev skills, and to easily gather all my socials into one place.
+
+## Extra Info
+If you want to ever showcase your Discord tag on this site, I wrote some code in preperation for that, so your entry should look like this for discord
+```json
+"discord": {
+    "user": "akira#6505",
+    "image": "https://example.com/favicon.ico"
+}
+```
--- script.js
+++ script.js
@@ -2,31 +2,72 @@ const socialLink = document.querySelector('#socialLink');
 const profile = document.querySelector('#profile');
 const title = document.querySelector('#title');
 const backgroundImg = document.querySelector('#main').style;
+const links = document.querySelector('#links');
 
 getJSON().then(data => {
-    
+
     backgroundImg.setProperty('--background', `url(${data.user.background})`);
     profile.src = data.user.profile;
     title.textContent = data.user.name;
 
     for (social in data.socials) {
         let items = Object.values(data.socials[social]);
+        if (social === "discord") {
+            const discordDiv = createDiscord(items);
+            discordDiv.addEventListener('click', (e) => {
+                navigator.clipboard.writeText(discordDiv.textContent);
+
+                const notification = document.querySelector('#discordPopUp');
+                notification.style.opacity = "100";
+                const closeButton = document.querySelector('#copyText > a')
+                closeButton.addEventListener('click', (e) => {
+                    notification.style.opacity = "0";
+                })
+            });
+            links.parentNode.insertBefore(discordDiv, links.nextSibling);
+            break;
+        }
         if (items[2] <= 0) { break }
-        const a = document.createElement("a");
-        const img = document.createElement("img");
-        a.href = items[1];
-        a.target = "_blank";
-        img.src = items[2];
-        img.alt = `${social} - ${items[0]}`
-        img.classList.add("socialLink");
-        a.appendChild(img);
+        const a = createAElement(items);
+        a.appendChild(createImgElement(items, social));
         socialLink.appendChild(a);
     }
 })
+
+
 
 async function getJSON() {
     const requestURL = './socials.json';
     const request = new Request(requestURL);
     const response = await fetch(request);
     return response.json();
+}
+
+function createAElement(data) {
+    const a = document.createElement("a");
+    a.href = data[1];
+    a.target = "_blank";
+    return a;
+}
+
+function createImgElement(data, extraInfo = "") {
+    const img = document.createElement("img");
+    img.src = data[2];
+    img.alt = `${extraInfo} - ${data[0]}`
+    img.classList.add("socialLink");
+    return img;
+}
+
+function createDiscord(data) {
+    const discordDiv = document.createElement("div");
+    const img = document.createElement("img");
+    const p = document.createElement('p');
+    discordDiv.id = "discord";
+    p.textContent = data[0];
+    img.src = data[1];
+    img.alt = `discord - ${data[0]}`;
+    img.classList.add('socialLink');
+    discordDiv.appendChild(img)
+    discordDiv.appendChild(p);
+    return discordDiv;
 }
